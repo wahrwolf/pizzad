b7cdaf9b472504c720b9bc9ed04a02862bf4fe3c
Vincent Dahmen <wahrwolf@wolfpit.net>
2023-11-06 13:03:42 +0100

    struture: split up everything into nice fiels

--- main.py
+++ main.py
@@ -2,33 +2,12 @@
 This is the main file of the flask application.
 It contains the routes and logic for calculating the number of pizzas needed.
 '''
-from flask import Flask, render_template, request
 from argparse import ArgumentParser
-from math import pi, ceil
+from math import ceil
+from flask import Flask
 
-app = Flask(__name__)
-
-# The small (16cm) pizza works for one person
-# This is a magic value learned by testing
-PIZZA_AREA_PER_PERSON = {
-        "snack": 16 ** 2 * pi,
-        "dinner": 25 ** 2 * pi,
-}
-
-VALID_PORTION_SIZES = [size for size in PIZZA_AREA_PER_PERSON.keys()]
-
-
-def calculate_pizzas(pizza_radius, num_people, portion_size=VALID_PORTION_SIZES[0]):
-    assert num_people >= 0, "Number of people needs to be positive."
-    assert pizza_radius >= 0, "Pizza radius needs to be positive."
-    assert portion_size in VALID_PORTION_SIZES, f"Portion size '{portion_size}' is not in {VALID_PORTION_SIZES}"
-
-    pizza_area = pizza_radius ** 2 * pi
-
-    pizza_area_needed = PIZZA_AREA_PER_PERSON[portion_size] * num_people
-    num_pizzas = pizza_area_needed / pizza_area
-
-    return num_pizzas
+from calculations import calculate_pizzas, VALID_PORTION_SIZES
+from webapp import blueprint
 
 
 def print_usage():
@@ -38,29 +17,6 @@ def print_usage():
         pizzactl serve          : spins up a server
     """
     print(help_message)
-
-
-@app.route('/')
-def home():
-    return render_template('index.html', valid_portion_sizes=VALID_PORTION_SIZES)
-
-
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    pizza_radius = float(request.form['radius'])
-    number_of_people = int(request.form['num_people'])
-    portion_size = str(request.form['portion_size'])
-
-    try:
-        num_pizzas = calculate_pizzas(
-                pizza_radius=pizza_radius,
-                num_people=number_of_people,
-                portion_size=portion_size
-        )
-    except Exception as error:
-        return render_template('error.html', message=str(error))
-
-    return render_template('result.html', num_pizzas=num_pizzas)
 
 
 def main():
@@ -81,7 +37,7 @@ def main():
     serve_parser.add_argument('--port', default=5000, type=int, help='Port name to listen for incomming request')
     serve_parser.add_argument('--host', default="localhost", type=str, help='Host name for the server to use')
     serve_parser.add_argument('--enable_debug', type=bool, help='Enables debug for the sever')
-    serve_parser.add_argument('--web_root', type=str, default='', help='The web root prefix if this is running behind a proxy')
+    serve_parser.add_argument('--url_prefix', type=str, default='', help='The proxy url prefix to use')
 
     args = parser.parse_args()
 
@@ -93,8 +49,10 @@ def main():
             else:
                 print(ceil(num_pizzas))
         case 'serve':
-            if args.web_root:
-                app.config["APPLICATION_ROOT"] = args.web_root
+            app = Flask(__name__)
+            if args.url_prefix:
+                app.config["APPLICATION_ROOT"] = args.url_prefix
+            app.register_blueprint(blueprint, url_prefix=args.url_prefix)
             app.run(host=args.host, port=args.port, debug=args.enable_debug)
         case _:
             print_usage()
