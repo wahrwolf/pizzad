36b1488c92e611641dc4e6cc0bce7621a08b82b4
Vincent Dahmen <wahrwolf@wolfpit.net>
2023-11-12 19:51:36 +0100

    food: adds ingredients + allergenes as concept

---
+++ pizzad/food/enums.py
@@ -0,0 +1,40 @@
+from enum import StrEnum, Enum, auto
+
+
+class Allergen(StrEnum):
+    MILK = auto()
+    GLUTEN = auto()
+    CRAB = auto()
+    EGG = auto()
+    FISH = auto()
+    PEANUT = auto()
+    SOJA = auto()
+    SESAM = auto()
+    MUSTARD = auto()
+    NUT = auto()
+    MOLLUSC = auto()
+    SULFAT = auto()
+    LUPIN = auto()
+
+    ANIMAL = auto()
+    TREIF = auto()
+    HARAM = auto()
+
+
+class Ingredient(Enum):
+    CHEESE = (Allergen.MILK,)
+
+    VEGETARIAN = (Allergen.MILK, Allergen.EGG)
+    MEAT = (Allergen. CRAB, Allergen.EGG, Allergen.FISH, Allergen.ANIMAL)
+
+    def __init__(self, *args: list[Allergen]):
+        self._allergenes = {arg for arg in args if isinstance(arg, Allergen)}
+
+    def contains_allergenes(self, *args: list[Allergen]):
+        for allergen in args:
+            if allergen in self._allergenes:
+                return True
+        return False
+
+    def get_allergenes(self) -> set[Allergen]:
+        return self._allergenes
